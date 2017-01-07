6e3347bd762d9934989eace312d1855c1d668f16
Erika Yasuda <eri.yasuda222@gmail.com>
2017-01-06 18:21:35 -0800

    Add comments to PHP

--- tip_calculator.php
+++ tip_calculator.php
@@ -1,3 +1,8 @@
+<?php
+## Erika Yasuda
+## Tip calculator that accepts a subtotal, a tip percent (options or custom percentage) and an optional split feature to split the total cost and tip between any specified number of people. 
+## 5 January 2017
+?>
 <!DOCTYPE html<html>
 <head>
 	<title>Tip Calculator</title>
@@ -6,7 +11,10 @@
 <body>
 	<h1>Tip Calculator</h1>
 			<form name="input" action="" method="post">
+				<!-- Asks for user to input subtotal cost -->
 	  			Bill subtotal $<input type="text" name="cost" value=<?php if(isset($_POST['cost'])) { echo $_POST['cost']; }?>><br>
+
+	  			<!-- Asks for user to select a tip with radio buttons or input a custom tip percentage into the text box -->
 				<p>Tip Percentage</p>
 				<?php
 				for($i = 10; $i < 21; $i+=5) {
@@ -17,11 +25,15 @@
 				?>
 				<input type="radio" name="tipval" value=0> Custom: <input type="text" id="custom" name="custom" value=<?php if(isset($_POST['custom'])) { echo $_POST['custom']; }?>>% <br>
 
+				<!-- Optional feature which allows users to split the total tip and cost by any specified number of people -->
 				Split: <input type="text" name="split" id="split" value=<?php if(isset($_POST['split'])) { echo $_POST['split']; }?>> person(s)<br>
-				
+
+				<!-- Submission button to submit form information -->
 				<input type="submit" name="submit" value="Submit">
 
 			<?php 
+				## Calculating and setting tip value and total value depending on the subtotal and tip percentage selected by the user
+				## Will print the respective values if all required forms are filled in and any submitted forms contain acceptable values
 				if (!empty($_POST['cost']) && $_POST['cost'] > 0.00 && ($_POST['custom'] > 0 || empty($_POST['custom']))) { 
 					if($_POST['tipval'] > 0) {
 						$tip = number_format((float)$_POST['cost'] * $_POST['tipval'], 2, '.', '');
@@ -31,11 +43,13 @@
 						$total = number_format((float)$_POST['cost'] + ($_POST['cost'] * $_POST['custom']/100), 2, '.', '');
 					}
 					?>
+						<!-- Total Tip and Total Cost to display -->
 						<div id="result" style="display: hide" >
 						<div>Tip: $<?php echo $tip ?></div>
 						<div>Total: $<?php echo $total ?></div>
 					<?php
 
+					## Individual tips and totals each member must pay if the split is greater than or equal to 2 persons
 					if(!empty($_POST['split'])) {
 						if($_POST['split'] > 1) {
 						?>
